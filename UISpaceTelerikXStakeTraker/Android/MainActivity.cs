5e64219a70752ef16806a1c4f2a43d742d74d79d
johnnyaby123 <yohannesabayneh548@gmail.com>
2020-12-01 18:10:48 +0300

    update the value

--- UISpaceTelerikXStakeTraker/Android/MainActivity.cs
+++ UISpaceTelerikXStakeTraker/Android/MainActivity.cs
@@ -8,7 +8,8 @@ using Android.OS;
 
 namespace UISpaceTelerikXStakeTraker.Android
 {
-    [Activity(Label = "UISpaceTelerikXStakeTraker.Android", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "UISpaceTelerikXStakeTraker.Android", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
